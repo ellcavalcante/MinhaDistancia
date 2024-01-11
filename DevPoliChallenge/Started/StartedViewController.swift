681558f7f8e8d7c52498fc3994fcbeced85a8c45
Ellington Cavalcante <ellcavalcante778@gmail.com>
2024-01-11 01:13:27 -0300

    primeira task concluída

--- DevPoliChallenge/Started/StartedViewController.swift
+++ DevPoliChallenge/Started/StartedViewController.swift
@@ -7,24 +7,69 @@
 //
 
 import UIKit
+import CoreLocation
 
 class StartedViewController: UIViewController {
     
     var screen: StartedScreen?
+
+    private let manager = CLLocationManager()
     
     override func loadView() {
         screen = StartedScreen()
         view = screen
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         screen?.delegate = self
+        setupLocationManager()
     }
+
+    private func setupLocationManager() {
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    }
+
+    private func requestLocationAuthorization() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
 }
 
 extension StartedViewController: StartedScreenProtocol {
     func actionEnableButton() {
+        requestLocationAuthorization()
         print(#function)
     }
 }
+
+extension StartedViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let status = manager.authorizationStatus
+        switch status {
+        case .notDetermined:
+            print("notDetermined")
+            manager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+        case .authorizedAlways:
+            print("authorizedAlways")
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+        @unknown default:
+            print("nothing")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        print("New location: \(location?.coordinate.latitude ?? 0), \(location?.coordinate.longitude ?? 0)")
+    }
+}
+
+
